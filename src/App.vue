e245d23841e0453327d49c83b164fd5299a1c1e4
giovana-rosa <giovana.rosaifc@gmail.com>
2025-04-22 09:30:53 -0300

    feat: adicionado pasta de img; início do header, main e footer (HTML).

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,38 @@
 
 <template>
   <header>
+    <nav>
 
+      <ul>
+        <li><a href="#"></a>Termos</li>
+        <li><a href="#"></a>Equipe</li>
+        <li><a href="#"></a>Envio</li>
+        <li><a href="#"></a>Devoluções</li>
+        <li><span class="fa-solid fa-cart-shopping" style="color: #26a269;"></span></li>
+        <li><span class="fa-solid fa-heart" style="color: #26a269;"></span></li>
+        <li><span class="fa-solid fa-user" style="color: #26a269;"></span></li>
+      </ul>
+    </nav>
   </header>
   <main>
-    <section>
+    <section class="banner">
+      <div>
 
+      </div>
+      <div class="livroBanner">
+        <img src="../public/img/livroBanner.png" alt="Livro do Banner">
+
+      </div>
     </section>
   </main>
   <footer>
     <div>
       <p>IFbooks</p>
-
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
     <div>
       
