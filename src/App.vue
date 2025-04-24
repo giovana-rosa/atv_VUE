7ec4c1369090b2613ade17731f2f6a929b13330a
giovana-rosa <giovana.rosaifc@gmail.com>
2025-04-24 07:38:14 -0300

    feat: ícones adicionados

--- src/App.vue
+++ src/App.vue
@@ -19,22 +19,32 @@
   </header>
   <main>
     <section class="banner">
-      <div>
-
+      <div class="introduçãoBanner">
+        <p>Autor de Abril</p>
+        <h1>Eric-Emanuel Schmitt</h1>
+        <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+        <button>Acessar página do livro</button>
       </div>
       <div class="livroBanner">
         <img src="../public/img/livroBanner.png" alt="Livro do Banner">
-
       </div>
+    </section>
+    <section>
+      <span class="fa-solid fa-truck"></span> Frete grátis para SC
+      <span class="fa-solid fa-star"></span> Livros recomendados
+      <span class="fa-solid fa-book-open"></span> Mais vendidos
+    </section>
+    <section class="lançamentos">
+      <h2>Lançamentos</h2>
     </section>
   </main>
   <footer>
     <div>
       <p>IFbooks</p>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><span class="fa-brands fa-square-facebook" style="color: #ffffff;"></span></li>
+        <li><span class="fa-brands fa-instagram" style="color: #ffffff;"></span></li>
+        <li><span class="fa-brands fa-square-twitter" style="color: #ffffff;"></span></li>
       </ul>
     </div>
     <div>
@@ -44,5 +54,12 @@
 </template>
 
 <style scoped>
+/*HEADER*/
 
+/*MAIN*/
+
+/*FOOTER*/
+footer {
+  background-color: #27AE60;
+}
 </style>
