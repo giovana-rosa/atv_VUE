c5b43476dcd270ab4d5b5df59bb70fe7757697b2
giovana-rosa <giovana.rosaifc@gmail.com>
2025-04-25 16:46:42 -0300

    feat: banner finalizado.

--- src/App.vue
+++ src/App.vue
@@ -37,18 +37,30 @@ const produtos = [
   <main>
     <section class="banner"> 
       <div class="introducaoBanner">
-        <p><span>Autor de Abril</span></p>
+        <p class="autor_abril"><span>Autor de Abril</span></p>
         <h1>Eric-Emanuel Schmitt</h1>
-        <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+        <p class="blocoDeTextoDoBanner">Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
         <button>Acessar página do livro</button>
       </div> 
       <div class="capaLivro">
         <img src="/img/livroBanner.png" alt="Livro do banner">
-        <p>*within the stock limit</p>
+        <p class="paragrafoAoLadoDaImagemLivro">*within the stock limit</p>
       </div>
     </section>
     <section class="informacoes">
-
+      <div class="iconesDeInformacaoLadoALado">
+        <div class="icone-truck">
+        <p><span class="fa-solid fa-truck icone-truck"></span>Frete grátis para SC</p>
+      </div>
+      <div class="icone-star">
+        <p><span class="fa-solid fa-star icone-star"></span>Livros recomendados</p>
+      </div>
+      <div>
+        <p><span class="fa-solid fa-book-open icone-book" ></span>Mais vendidos</p>
+      </div>
+      </div>
+    
+      
     </section> 
     <section class="lancamentos">
       <h2>Lançamentos</h2>
@@ -92,11 +104,12 @@ const produtos = [
 
 <style scoped>
 /*HEADER*/
-#divPesquisa {
+  #divPesquisa {
   position: relative;
   display: inline-block;
 }
-#textotBarraDePesquisa {
+
+  #textotBarraDePesquisa {
   padding-right: 35px; /* espaço pra lupa não ficar em cima do texto */
   width: 376.52px;
   height: 36.05px;
@@ -108,7 +121,8 @@ const produtos = [
   color: #000000;
   font-family: Arial, Helvetica, sans-serif;
 }
-#lupa {
+
+  #lupa {
   position: absolute;
   right: 5px;
   top: 50%;
@@ -119,53 +133,170 @@ const produtos = [
   color: #231F2D;
   font-size: 16px;
 }
-header {
+
+  header {
   display: flex;
   margin: 2vw 2vw 2vw 3vw;
+  justify-content: center;
+}
+
+  header  {
   border-bottom: 1px solid #27AE60;
 }
-header p {
+
+  header p div {
+  border-bottom: 1px solid #27AE60;
+}
+
+  header p {
   text-align: center; 
 }
-header span.IFbook {
+
+  header span.IFbook {
   border-right: 1px solid #27AE60;
-  margin: 1vw ;
+  margin: 0 0 0 10px;
+ 
 }
-header p.apreco_leitura {
+
+  header p.apreco_leitura {
   color: #27AE60;
-  margin: 0 2vw 0 0;
+  margin: 0 20px 0 10px;
 }
-header ul {
+
+  header ul {
   display: flex;
 }
-header nav ul li {
+
+  header nav ul li {
   margin: 10px 30px 10px 30px;
 }
-.icone {
+
+  .icone {
   color: #27AE60;
   border-right: 1px solid #27AE60;
 }
-.icone-user {
+
+  .icone-user {
   color: #27AE60;
 }
 
 /*MAIN*/
 
-div.capaLivro {
+  section.banner {
   border-bottom: 1px solid #27AE60;
+} 
+
+  section.banner  {
+  display: flex;
 }
-div.introducaoBanner span {
+
+  div.introducaoBanner span {
   border: 1px solid #27AE60;
   padding: 10.01px;
   border-radius: 3px;
   color: #27AE60;
 }
-.lancamentos {
+
+section.banner div.introducaoBanner p.autor_abril {
+  margin: 8vw 2vw 2vw 17vw;
+  
+}
+
+section.banner div.introducaoBanner h1 {
+  font-size: 48px;
+  margin: 0 2vw 0 17vw;
+  
+}
+
+section.banner div.introducaoBanner p.blocoDeTextoDoBanner {
+  color: #4D4C4C;
+  font-size: 16px;
+  margin: 3vw 0 2vw 17vw;
+  width: 477.6625061035156px;
+  line-height: 24.03px;
+  letter-spacing: 0%;
+  font-weight: 400;
+}
+
+section.banner div.introducaoBanner button {
+  color: white;
+  font-size: 16.02px;
+  width: 179px;
+  height: 25px;
+  font-weight: 500;
+  line-height: 24.03px;
+  letter-spacing: 0%;
+  width: 243.08889770507812px;
+  height: 49.03333282470703px;
+  top: 516.85px;
+  left: 164.23px;
+  border-radius: 2px;
+  top: 12.02px;
+  right: 32.04px;
+  bottom: 12.02px;
+  left: 32.04px;
+  gap: 10.01px;
+  background-color: #27AE60;
+  margin: 0 2vw 0 17vw;
+  
+}
+
+  section.banner div.capaLivro img{
+    margin: 0 0 0 10vw;
+  }
+
+  section.banner div.capaLivro .paragrafoAoLadoDaImagemLivro {
+    width: 168.23333740234375px;
+    height: 25px;
+    top: 586.95px;
+    left: 1150.61px;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24.03px;
+    letter-spacing: 0%;
+    margin: 0 0 2vw 25vw ;
+  }
+
+  
+  section.informacoes div.icone-book p span  {
+    width: 38.453304290771484px;
+    height: 33.646644592285156px;
+    top: 9.61px;
+    left: 4.81px;
+    display: flex;
+  
+  }
+
+  section.infomacao div.conesDeInformacaoLadoLado {
+    display: flex;
+  }
+  
+  section.informacoes div.icone-truck p span  {
+    width: 38.453304290771484px;
+    height: 33.646644592285156px;
+    top: 9.61px;
+    left: 4.81px;
+    display: flex;
+  
+  }
+
+  section.informacoes div.icone-star p span  {
+    width: 38.453304290771484px;
+    height: 33.646644592285156px;
+    top: 9.61px;
+    left: 4.81px;
+    display: flex;
+  
+  }
+
+  .lancamentos {
+  
   padding: 0 0 15vw 0;
 }
 
+
 /*FOOTER*/
-.footer {
+  .footer {
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
@@ -173,36 +304,43 @@ div.introducaoBanner span {
   background-color: #27AE60;
   color: white;
 }
-.esquerdo {
+
+  .esquerdo {
   display: flex;
   flex-direction: column;
   gap: 15px;
 }
-.direito {
+
+  .direito {
   display: flex;
   flex-direction: column;
   gap: 15px;
 }
-.logo {
+
+  .logo {
   font-size: 1.5rem;
   font-weight: bold;
 }
-.icones {
+
+  .icones {
   list-style: none;
   display: flex;
   gap: 15px;
   padding: 0;
   margin: 0;
 }
-.contato p {
+
+  .contato p {
   margin: 0;
   line-height: 1.6;
 }
-.cartoes img {
+
+  .cartoes img {
   width: 50px;
   margin-right: 10px;
 }
-.direitos {
+
+  .direitos {
   text-align: center;
   padding: 15px;
   font-size: 0.9rem;
