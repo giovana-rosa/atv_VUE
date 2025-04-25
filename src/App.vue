4d24d15e2512b533f0bbe97442829233e8aa9a39
giovana-rosa <giovana.rosaifc@gmail.com>
2025-04-25 13:12:26 -0300

    feat: reset adicionado na main.css

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,6 @@
 <script setup>
-const produtos = [
-    {
-        id: 1,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Chain of Iron: Volume 2',
-        autor: 'Cassandra Clare',
-        preco: 23.24,
-    },
-    {
-        id: 2,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Chain of Thorns',
-        autor: 'Cassandra Clare',
-        preco: 23.24,
-    },
-    {
-        id: 3,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'City of Fallen Angels',
-        autor: 'Cassandra Clare',
-        preco: 13.94,
-    },
-    {
-        id: 4,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Nona the Ninth',
-        autor: 'Cassandra Clare',
-        preco: 16.84,
-    },
-    {
-        id: 5,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Harlem Shuffle',
-        autor: 'Colson Whitehead',
-        preco: 26.92,
-    },
-    {
-        id: 6,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Two Old Women',
-        autor: 'Velma Wallis',
-        preco: 13.95,
-    },
-    {
-        id: 7,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Carrie Soto Is Back',
-        autor: 'Taylor Jenkins Reid',
-        preco: 26.04,
-    },
-    {
-        id: 8,
-        capa: 'https://placehold.co/600x400.png',
-        titulo: 'Book Lovers',
-        autor: 'Emily Henry',
-        preco: 15.81,
-    },
-];
 
-const items = [
+const produtos = [
     { id: 1, capa: '/img/Chain_of_Iron_Vol.2.png', titulo: 'Chain of Iron: Volume 2', autor: 'Cassandra Clare', preco: '23.24'},
     { id: 2, capa: '/img/Chain_of_Thorns.png', titulo: 'Chain of Thorns', autor: 'Cassandra Clare', preco: '23.24'},
     { id: 3, capa: '/img/City_of_Fallen_Angels.png', titulo: 'City of Fallen Angels', autor: 'Cassandra Clare', preco: '13.94'},
@@ -72,8 +14,8 @@ const items = [
 
 <template>
   <header>
-    <p>IFbooks <span>|</span></p>
-    <p>Apreço a <br>
+    <p>IFbooks<span class="IFbook"></span></p>
+    <p class="apreco_leitura">Apreço a <br>
         leitura</p>
         <div id="divPesquisa">
           <input type="text" id="textotBarraDePesquisa" placeholder="Pesquisar" />
@@ -85,14 +27,15 @@ const items = [
         <li><a href="#"></a>Equipe</li>
         <li><a href="#"></a>Envio</li>
         <li><a href="#"></a>Devoluções</li>
-        <li><span class="fa-solid fa-cart-shopping" style="color: #26a269;">|</span></li>
-        <li><span class="fa-solid fa-heart" style="color: #26a269;">|</span></li>
-        <li><span class="fa-solid fa-user" style="color: #26a269;"></span></li>
+        <li><span class="fa-solid fa-cart-shopping icone"></span></li>
+        <li><span class="fa-solid fa-heart icone"></span></li>
+        <li><span class="fa-solid fa-user icone-user"></span></li>
       </ul>
     </nav>
+
   </header>
   <main>
-    <section class="banner">
+    <section class="banner"> 
       <div class="introducaoBanner">
         <p><span>Autor de Abril</span></p>
         <h1>Eric-Emanuel Schmitt</h1>
@@ -110,44 +53,51 @@ const items = [
     <section class="lancamentos">
       <h2>Lançamentos</h2>
       <ul>
-        <li v-for="item in items" :key="item.id">
-        <img :src="item.capa" alt="Capa do livro" />
-        {{ item.titulo }} {{ item.autor }} {{ 'R$ ' + Number(item.preco).toFixed(2).replace('.', ',') }}
+        <li v-for="produto in produtos" :key="produto.id">
+        <img :src="produto.capa" alt="Capa do livro" />
+        {{ produto.titulo }} {{ produto.autor }} {{ 'R$ ' + Number(produto.preco).toFixed(2).replace('.', ',') }}
         </li>
       </ul>
     </section>
   </main>
   <footer>
-    <div>
-      <p>IFbooks</p>
-      <ul>
+  <div class="footer">
+    <div class="esquerdo">
+      <p class="logo">IFbooks</p>
+      <ul class="icones">
         <li><span class="fa-brands fa-square-facebook" style="color: #ffffff;"></span></li>
         <li><span class="fa-brands fa-instagram" style="color: #ffffff;"></span></li>
         <li><span class="fa-brands fa-square-twitter" style="color: #ffffff;"></span></li>
       </ul>
     </div>
-    <div>
-      <p>Contato</p>
-      <span class="fa-solid fa-phone" style="color: #FFFFFFCC;"></span> +55 47 40045263
-      <span class="fa-solid fa-clock" style=" color: #FFFFFFCC;"></span> 8h às 23h - Seg a Sex
-      <span class="fa-solid fa-envelope" style=" color: #FFFFFFCC;"></span> contato@ifbooks.com
+    <div class="direito">
+      <div class="contato">
+        <p>Contato</p>
+        <p>
+          <span class="fa-solid fa-phone" style="color: #FFFFFFCC;"></span> +55 47 40045263<br>
+          <span class="fa-solid fa-clock" style="color: #FFFFFFCC;"></span> 8h às 23h - Seg a Sex<br>
+          <span class="fa-solid fa-envelope" style="color: #FFFFFFCC;"></span> contato@ifbooks.com
+        </p>
+      </div>
+      <div class="cartoes">
+        <img src="/img/paypal_card.png" alt="Cartão PAYPAL">
+        <img src="/img/Master_card.png" alt="Cartão MasterCard">
+        <img src="/img/VISA_card.png" alt="Cartão VISA">
+      </div>
     </div>
-    <div>
-      <img src="/img/paypal_card.png" alt="Cartão PAYPAL">
-      <img src="/img/Master_card.png" alt="Cartão MasterCard">
-      <img src="/img/VISA_card.png" alt="Cartão VISA">
-    </div>
-    <p>&copy; Alguns direitos reservados. IFbooks 2025. </p>
-  </footer>
+  </div>
+  <p class="direitos">&copy; Alguns direitos reservados. IFbooks 2025.</p>
+</footer>
 </template>
 
 <style scoped>
+
+
 /*HEADER*/
 #divPesquisa {
   position: relative;
   display: inline-block;
 }
-
 #textotBarraDePesquisa {
   padding-right: 35px; /* espaço pra lupa não ficar em cima do texto */
   width: 376.52px;
@@ -159,9 +109,7 @@ const items = [
   background-color: #F1F1F1;
   color: #000000;
   font-family: Arial, Helvetica, sans-serif;
-
 }
-
 #lupa {
   position: absolute;
   right: 5px;
@@ -173,19 +121,94 @@ const items = [
   color: #231F2D;
   font-size: 16px;
 }
+header {
+  display: flex;
+  margin: 2vw 2vw 2vw 3vw;
+  border-bottom: 1px solid #27AE60;
+}
+header p {
+  text-align: center; 
+}
+header span.IFbook {
+  border-right: 1px solid #27AE60;
+  margin: 1vw ;
+}
+header p.apreco_leitura {
+  color: #27AE60;
+  margin: 0 2vw 0 0;
+}
+header ul {
+  display: flex;
+}
+header nav ul li {
+  margin: 10px 30px 10px 30px;
+}
+.icone {
+  color: #27AE60;
+  border-right: 1px solid #27AE60;
+}
+.icone-user {
+  color: #27AE60;
+}
 
 /*MAIN*/
 
+div.capaLivro {
+  border-bottom: 1px solid #27AE60;
+}
 div.introducaoBanner span {
   border: 1px solid #27AE60;
   padding: 10.01px;
   border-radius: 3px;
   color: #27AE60;
 }
+.lancamentos {
+  padding: 0 0 15vw 0;
+}
 
 /*FOOTER*/
-
-footer {
+.footer {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 20px;
   background-color: #27AE60;
+  color: white;
+}
+.esquerdo {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.direito {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+.logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.icones {
+  list-style: none;
+  display: flex;
+  gap: 15px;
+  padding: 0;
+  margin: 0;
+}
+.contato p {
+  margin: 0;
+  line-height: 1.6;
+}
+.cartoes img {
+  width: 50px;
+  margin-right: 10px;
+}
+.direitos {
+  text-align: center;
+  padding: 15px;
+  font-size: 0.9rem;
+  background-color: #27AE60; 
+  color: #FFFFFF99;
 }
 </style>
